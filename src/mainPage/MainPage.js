ad1846a7472f07a012580b3dbc625f18431feff6
KimYoonChae <22400147@handong.ac.kr>
2025-08-12 00:08:51 +0900

    style:종목조회버튼추가

--- src/mainPage/MainPage.js
+++ src/mainPage/MainPage.js
@@ -1,13 +1,14 @@
 import Header from "../common/Header";
 import Sidebar from "../common/Sidebar";
 import Testpage from "../testPage/TestPage";
-
+import Typebtn from "./component/typebtn";
 export default function MainPage() {
   return (
     <div className="container">
       <Header />
       <Sidebar />
-        <Testpage />
+      <Typebtn/>
+       
     </div>
   );
 }
