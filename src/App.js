6965bd9277d7ed2c8cc70c2c1fae14abcbb32310
junnyang <lhr8440@gmail.com>
2025-08-14 19:53:10 +0900

    권한 업로드

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./mainPage/MainPage";
 import CenterDetailPage from "./centerdetailPage/CenterDetailPage";
+import GatherPage from "./joinPage/GatherPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/detail/:id" element={<CenterDetailPage />} />
+        <Route path="/gather/:id" element={<GatherPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/joinPage/GatherPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "../common/Header";
+
+export class GatherPage {
+  render() {
+    return (
+      <div>
+        <Header></Header>
+      </div>
+    );
+  }
+}
+
+export default GatherPage;
