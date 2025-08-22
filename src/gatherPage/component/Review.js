7f8a235b416ac77052c97067e0e128558991dc1d
junnyang <lhr8440@gmail.com>
2025-08-22 22:40:12 +0900

    feat: review 창에 이름 띄우기

--- src/gatherPage/component/Review.js
+++ src/gatherPage/component/Review.js
@@ -26,8 +26,31 @@ function TimeFormat(time) {
   if (diffDays === 1) return "1일 전";
   return `${diffDays}일 전`;
 }
+function parseJwt(token) {
+  try {
+    const base64Url = token.split(".")[1];
+    //JWT는 헤더.페이로드.서명 구조인데 점(.)을 기준으로 하여 페이로드를 꺼낸다
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    //JWT 페이로드는 Base64URL 형식이라서 -,_를 쓰는데 브라우저의 atob가 이해하도록
+    //Base64로 바꾸기 위해 - -> +, _ -> /로 치환한다.
+
+    //정상적인 문자열(UTF-8)로 복원하기 기능
+    const jsonPayload = decodeURIComponent(
+      atob(base64) //Base64 문자열을 바이너리 문자열로 디코딩
+        .split("") //문자열을 문자 배열로 나누기
+        .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+        //각 문자의 코드 값을 16진수 2자리로 변환하고 앞에 %를 붙여서 인코딩함.
+        .join("") //인코딩 조각들을 하나의 문자열로 결합하기
+    );
+
+    return JSON.parse(jsonPayload); //JSON문자열을 자바스크립트 객체로 파싱해서 반환함
+  } catch (e) {
+    return null;
+  }
+}
 
 function Review({ userName, isLoggedIn }) {
+  isLoggedIn = true; // 임시로 로그인 상태로 설정
   const { id } = useParams(); // /gather/:id 같은 라우트에서 id 추출
 
   const [reviews, setReviews] = useState([]);
@@ -37,7 +60,22 @@ function Review({ userName, isLoggedIn }) {
   const [content, setContent] = useState("");
   const [secret, setSecret] = useState(0);
   const [posting, setPosting] = useState(false);
+  const [userNamee, setUserName] = useState("");
   isLoggedIn = true;
+
+  //사용자 이름 받기
+  useEffect(() => {
+    const token = localStorage.getItem("token"); //로컬스토리지에서 token으로 저장된 jwt 문자열 읽기
+    //토큰이 존재하면,
+    if (token) {
+      const payload = parseJwt(token); //디코딩으로 페이로드 객체 얻기
+
+      //페이로드가 존재하고, 그 안에 username 속성이 있을때 진행하기
+      if (payload?.username) {
+        setUserName(payload.username); //가져온 사용자명 상태로 저장하기
+      }
+    }
+  }, [userName]);
 
   // ✅ 댓글 목록 불러오기
   useEffect(() => {
@@ -82,7 +120,7 @@ function Review({ userName, isLoggedIn }) {
       setPosting(true);
       setErr(null);
       const payload = {
-        id: Number(id), // 모임 아이디
+        id: id, // 모임 아이디
         content: content.trim(),
         secrete: secret, // 0 or 1
       };
@@ -92,7 +130,7 @@ function Review({ userName, isLoggedIn }) {
       // 새 댓글을 목록에 추가 (서버에서 새 객체 반환 시 data로 교체)
       setReviews((prev) => [
         {
-          id: data.id ?? `${userName}-${Date.now()}`,
+          id: id,
           name: userName,
           time: new Date().toISOString(),
           comment: content.trim(),
@@ -137,7 +175,7 @@ function Review({ userName, isLoggedIn }) {
       {/* 작성 영역 */}
       <div className={styles.write}>
         <div className={styles.writeHeader}>
-          {isLoggedIn ? `${userName}님` : ""}
+          {isLoggedIn ? `${userNamee}님` : ""}
         </div>
         <textarea
           className={styles.textarea}
