59dbb14833da93ae1d4e27863bf759491cfbe735
junnyang <lhr8440@gmail.com>
2025-08-25 02:04:09 +0900

    퇴근원해요? 네

--- src/gatherPage/component/List.js
+++ src/gatherPage/component/List.js
@@ -1,5 +1,5 @@
 // List.jsx
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import fallbackData from "../../data/gather.json";
